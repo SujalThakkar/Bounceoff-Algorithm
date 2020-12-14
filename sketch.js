e63d28176aea5592b6296c73d63552d8439e8f02
SujalThakkar <75084232+SujalThakkar@users.noreply.github.com>
2020-12-14 17:55:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+//declare variables
+var fixedRect, movingRect;
+
+function setup() {
+  //creating canvas
+  createCanvas(1200,800);
+  //to create fixes rect sprite
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(600,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  movingRect.velocityY = 1;
+}
+
+function draw() {
+  background(0,0,0);  
+  //movingRect.x = World.mouseX;
+ // movingRect.y = World.mouseY;
+
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+    && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
+   ) {
+    movingRect.velocityX = movingRect.velocityX*(-1);
+    fixedRect.velocityX = fixedRect.velocityX*(-1);
+  }
+  if( movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
+    movingRect.velocityY = movingRect.velocityY*(-1);
+    fixedRect.velocityY = fixedRect.velocityY*(-1);
+  }
+  drawSprites();
+}
